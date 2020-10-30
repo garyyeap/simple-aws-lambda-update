17620f0deec1a353b6ef0ae3748d0f1e21986140
Gary Yeap <contact@garyyeap.com>
2020-10-30 20:56:07 +0800

    Tune operation console messages

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const shell = require('shelljs');
 const pack = require('npm-pack-all-zip');
 
 module.exports = async function (name, keep, devDeps) {
-  shell.echo('Updating codes to lambda...');
-
   if (!shell.which('aws')) {
     shell.echo('Sorry, this script requires aws-cli: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html');
     shell.exit(1);
@@ -13,7 +11,10 @@ module.exports = async function (name, keep, devDeps) {
 
   try {
     name = name || require(path.join(process.cwd(), 'package.json')).name;
-  } catch (e) {}
+  } catch (e) {
+    shell.echo('package.json not found or is invalid.\n Please check your current execution path or package.json file');
+    shell.exit(1);
+  }
 
   if (!name) {
     shell.echo('The name of the lambda function is required!');
@@ -26,6 +27,8 @@ module.exports = async function (name, keep, devDeps) {
     shell.echo(e);
     shell.exit(1);
   }
+
+  shell.echo(`Updating codes to lambda: ${name}`);
 
   const file = `fileb://./${name}.zip`;
   const command = `aws lambda update-function-code --function-name=${name} --zip-file=${file}`;
